051b2a3c2c86c7927ad679605de2c7326b42a89d
rian arif rahman <boxkaleng@gmail.com>
2026-05-04 23:02:19 +0700

    feat(prisma): add adapter prismaPg to prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,19 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from 'generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL,
+        });
+        super({ adapter })
+    }
+
   async onModuleInit() {
     try {
       await this.$connect();
