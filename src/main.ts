3f2a764378a4652048efe2c8af764b1e9667e283
rian arif rahman <boxkaleng@gmail.com>
2026-05-04 23:24:55 +0700

    feat(docs): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,14 +15,22 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
 
-  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('QNA API')
+    .setDescription('API for QNA application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`🚀 QnA API running on http://localhost:${port}/api`);
 }
 bootstrap().catch((error) => {
   console.error('Failed to start server:', error);
   process.exit(1);
-});
+}); 
